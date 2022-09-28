c1ee06ec57cf9965055838424572a4d41523ee10
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-09-28 12:14:15 +0300

    Task19 added

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Task 19
+void CheckForPali()
+{
+    Console.WriteLine("Hello, eneter palindrome number");
+    int number = int.Parse(Console.ReadLine());
+    if (number/10000 == number%10 && (number/1000)%10 == (number%100)/10)
+        {
+         Console.WriteLine("Number is palindrome");  
+        }
+    else{
+        Console.WriteLine("Number is not palindrome");
+    }
+}
+
+
+//Task
